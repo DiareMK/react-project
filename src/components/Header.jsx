de1bdb5b91b06c63dc6d2aeeb53daa33b305b264
DiareMK <DiareMK@users.noreply.github.com>
2026-03-04 14:21:14 +0200

    add header & heading components

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import Heading from "./Heading";
+
+function Header() {
+  return (
+    <header>
+      <Heading level={1} title=" 22 вивчає React" />
+    </header>
+  );
+}
+
+export default Header;
