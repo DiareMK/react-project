8cb0a5ab6ad8d994af73897a859715c42ac4bcf5
DiareMK <DiareMK@users.noreply.github.com>
2026-03-10 16:40:36 +0200

    Complete Laboratory Work 1: Add components and report

---
+++ src/components/atoms/Input/Input.jsx
@@ -0,0 +1,12 @@
+﻿import styles from './Input.module.css';
+
+const Input = ({ type = 'text', placeholder, label }) => {
+  return (
+    <div className={styles.container}>
+      {label && <label className={styles.label}>{label}</label>}
+      <input type={type} placeholder={placeholder} className={styles.input} />
+    </div>
+  );
+};
+
+export default Input;
