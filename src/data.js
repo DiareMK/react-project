322a5b276bbcc253b3d69536c02285296044335c
DiareMK <DiareMK@users.noreply.github.com>
2026-03-11 14:50:57 +0200

    Complete Practical Work 2: Array Transformations

--- src/data.js
+++ src/data.js
@@ -1,26 +1,35 @@
-export const postsData = [
+﻿export const postsData = [
   {
     id: 1,
     author: 'User123',
     avatar: 'https://placehold.co/50',
-    content: 'е мій перший пост у новій соціальній мережі! React - це круто.',
-    date: '2 год тому',
+    content: 'Рµ РјС–Р№ РїРµСЂС€РёР№ РїРѕСЃС‚ Сѓ РЅРѕРІС–Р№ СЃРѕС†С–Р°Р»СЊРЅС–Р№ РјРµСЂРµР¶С–! React - С†Рµ РєСЂСѓС‚Рѕ.',
+    date: '2 РіРѕРґ С‚РѕРјСѓ',
     likes: 5
   },
   {
     id: 2,
     author: 'Admin',
     avatar: 'https://placehold.co/50',
-    content: 'Сьогодні ми вивчаємо Lists & Keys. е забувайте про унікальні ключі!',
-    date: '4 год тому',
+    content: 'РЎСЊРѕРіРѕРґРЅС– РјРё РІРёРІС‡Р°С”РјРѕ Lists & Keys. Рµ Р·Р°Р±СѓРІР°Р№С‚Рµ РїСЂРѕ СѓРЅС–РєР°Р»СЊРЅС– РєР»СЋС‡С–!',
+    date: '4 РіРѕРґ С‚РѕРјСѓ',
     likes: 12
   },
   {
     id: 3,
     author: 'Student_KP',
     avatar: 'https://placehold.co/50',
-    content: 'абораторна робота 2 виконується успішно.',
-    date: '1 день тому',
+    content: 'Р°Р±РѕСЂР°С‚РѕСЂРЅР° СЂРѕР±РѕС‚Р° 2 РІРёРєРѕРЅСѓС”С‚СЊСЃСЏ СѓСЃРїС–С€РЅРѕ.',
+    date: '1 РґРµРЅСЊ С‚РѕРјСѓ',
     likes: 2
   }
 ];
+export const studentsData = [
+  { id: 1, name: 'лександр', score: 85, isActive: true },
+  { id: 2, name: 'арія', score: 55, isActive: true },
+  { id: 3, name: 'ван', score: 92, isActive: false },
+  { id: 4, name: 'нна', score: 78, isActive: true },
+  { id: 5, name: 'етро', score: 45, isActive: false },
+  { id: 6, name: 'лена', score: 95, isActive: true }
+];
+
